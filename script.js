9f5d473e6903c37b00e81de06f1b460d9002655c
Gilbert Smith <gjsmith3rd@gmail.com>
2016-02-25 23:15:58 -0600

    Processing array of users.

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 /* global $ */
 
-$(document).ready(function() {
+$(document).ready(function () {
+
+  var testUsers = ['freecodecamp', 'gjsmith3rd', 'ogamingsc2', 'brunofin', 'comster404', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff'];
+  var testUserData = [];
+
+  $.ajaxSetup({
+
+    headers: {
+      'Accept': 'application/vnd.twitchtv.v3+json'
+    }
+  });
+
+  testUsers.forEach(function (element, index, array) {
+
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + testUsers[element], function (data) {
+      console.log('https://api.twitch.tv/kraken/streams/' + element);
+      console.log(data);
+      testUserData.push(data);
+
+    });
+
+  });
+
+  console.log(testUserData);
 
 });
