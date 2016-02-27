0b25b4cae8ba13ddc42f5b0ddd2c2deb62c76082
Gilbert Smith <gjsmith3rd@gmail.com>
2016-02-26 19:34:15 -0600

    Finished.

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 
 $(document).ready(function () {
 
-  var testUsers = ['freecodecamp', 'gjsmith3rd', 'ogamingsc2', 'brunofin', 'comster404', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff'];
-  var testUserData = [];
+  var userGroup = ['freecodecamp', 'gjsmith3rd', 'ogamingsc2', 'brunofin', 'comster404', 'storbeck', 'terakilobyte', 'habathcx', 'robotcaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff'];
 
   $.ajaxSetup({
 
@@ -12,17 +11,69 @@ $(document).ready(function () {
     }
   });
 
-  testUsers.forEach(function (element, index, array) {
+  userGroup.sort().forEach(function (element, index, array) {
 
-    $.getJSON('https://api.twitch.tv/kraken/streams/' + element + '?callback=?', function (data) {
-      console.log('https://api.twitch.tv/kraken/streams/' + element);
-      console.log(data);
-      testUserData.push(data);
+    $.ajax({
+      url: 'https://api.twitch.tv/kraken/channels/' + element + '?callback=?',
+      dataType: 'json',
+
+      success: function (data) {
+
+        console.log(data);
+
+        if (data.status !== 422) {
+
+          var alertClass = data.status !== null ? 'alert-success' : 'alert-default';
+          var streamContent = data.status !== null ? 'now streaming' : 'stream offline';
+          var streamStatus = data.status !== null ? data.status : '';
+          var logo = data.logo !== null ? data.logo : 'http://i.imgur.com/5Ia41S4.png';
+
+          $('<li class="list-group-item">' +
+            '<img width="25" height="25" src="' +
+            logo +
+            '" />' +
+            ' ' +
+            '<strong>' +
+            data.display_name +
+            ' : ' +
+            '</strong>' +
+            '<a href="' +
+            data.url +
+            '">' +
+            streamStatus +
+            '</a>' +
+            '<span class="badge ' +
+            alertClass +
+            '">' +
+            streamContent +
+            '</span>' +
+            '</li>')
+            .appendTo('#listGroup');
+
+        } else {
+
+          console.log(element);
+
+          $('<li class="list-group-item">' +
+            element +
+            '<span class="badge alert-danger">' +
+            'account invalid' +
+            '</span>' +
+            '</li>')
+            .appendTo('#listGroup');
+
+        }
+
+      },
+
+      error: function (data) {
+
+        console.log('ERROR: ', data);
+
+      }
 
     });
 
   });
-
-  console.log(testUserData);
 
 });
