37dc2955c56e94661fd5019ce389f83b41623c75
Gilbert Smith <gjsmith3rd@gmail.com>
2016-05-22 21:06:06 -0500

    Changed images using cloudinary urls and added code to remove adsense.

--- source/srcscript.js
+++ source/srcscript.js
@@ -2,6 +2,20 @@
 
 $(document).ready(function () {
 
+  var stopGoogleAds = false;
+
+  //Test for local dev network
+  if (/^10.0.0/.test(location.hostname) && stopGoogleAds) {
+
+    // Remove Adsense from DOM
+    $('.adsense').remove();
+
+  } else {
+
+    // Load Adsense JavaScript
+    $.getScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+  }
+
   var userGroup = ['freecodecamp', 'gjsmith3rd', 'ogamingsc2', 'brunofin', 'comster404', 'storbeck', 'terakilobyte', 'habathcx', 'robotcaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff'];
 
   $.ajaxSetup({
@@ -24,7 +38,7 @@ $(document).ready(function () {
           var alertClass = data.status !== null ? 'alert-success' : 'alert-default';
           var streamContent = data.status !== null ? 'now streaming' : 'stream offline';
           var streamStatus = data.status !== null ? data.status : '';
-          var logo = data.logo !== null ? data.logo : 'http://res.cloudinary.com/mobilecreature/image/upload/v1456543488/FreeCodeCamp/Ziplines/Twitch-TV/5Ia41S4.png';
+          var logo = data.logo !== null ? data.logo : 'http://res.cloudinary.com/mobilecreature/image/upload/f_auto,fl_lossy,dpr_auto,q_80,w_35,h_35/v1456543488/FreeCodeCamp/Ziplines/Twitch-TV/5Ia41S4.png';
 
           $('<li class="clearfix list-group-item">' +
             ' ' +
@@ -60,7 +74,7 @@ $(document).ready(function () {
             '<br>' +
 
             '<img width="35" height="35" src="' +
-            'http://res.cloudinary.com/mobilecreature/image/upload/v1456607682/FreeCodeCamp/Ziplines/Twitch-TV/images.jpg' +
+            'http://res.cloudinary.com/mobilecreature/image/upload/f_auto,fl_lossy,dpr_auto,q_80,w_35,h_35/v1456607682/FreeCodeCamp/Ziplines/Twitch-TV/images.jpg' +
             '" />' +
             '<span class="badge alert-danger">' +
             'account invalid' +
